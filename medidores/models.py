e5ac913d29f38eafa553726c965af1c149aded6d
Fede Sabadini <fede-sabadini@protonmail.com>
2023-01-17 18:21:50 -0300

    Creo app medidores, que contiene los modelos Medidor y Medidcion. Creo migraciones.

---
+++ medidores/models.py
@@ -0,0 +1,40 @@
+"""Modelos de la aplicación Medidores."""
+# Python
+import uuid
+
+# Django
+from django.db import models
+from django.core.validators import MinValueValidator, RegexValidator
+
+alphanumeric = RegexValidator(
+    r'^[0-9a-zA-Z]*$', 
+    'Only alphanumeric characters are allowed.'
+)
+
+class Medidor(models.Model):
+    """ Modelo que representa un medidor."""
+    id_key = models.CharField(
+        primary_key=True,
+        unique=True,
+        max_length=10,
+        validators=[alphanumeric])
+    nombre = models.CharField(
+        max_length=20,
+        blank=False,
+        null=False,
+    )
+
+class Medicion(models.Model):
+    """ Modelo que representa la medición registrada en kwh de un medidor."""
+    medidor = models.ForeignKey(
+        Medidor,
+        blank=False,
+        null=False,
+        on_delete=models.PROTECT
+    )
+    fecha_hora = models.DateTimeField(auto_now_add=True)
+    consumo = models.DecimalField(
+        validators=[MinValueValidator(0)],
+        max_digits=12,
+        decimal_places=2
+    ) 
